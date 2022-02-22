d534f680f97cc019203721d73edfbc94d8c5673c
ahmedwal8d <85419960+ahmedwal8d@users.noreply.github.com>
2022-02-22 15:29:33 +0200

    commit

--- asseds/js/main.js
+++ asseds/js/main.js
@@ -3,22 +3,17 @@ let HeaderIcon =document.getElementById("header-icon");
 let HeaderIcons = document.getElementById("Header-Icons");
 let UlLinks = document.getElementById("links");
 let UlLink = document.getElementById("link")
+let GoverBox = document.querySelectorAll("#gover-box")
 
 icon.onclick = function () {
     document.body.classList.toggle("dark-thems")
 }
 
 HeaderIcon.addEventListener("click" , function () {
-    HeaderIcons.style.display = "flex"
-    HeaderIcons.style.position = "absolute"
-    HeaderIcons.style.right = "147px"
-    UlLinks.style.display = "flex"
-    UlLinks.style.position = "absolute"
-    UlLinks.style.left = "0"
-    UlLinks.style.top = "34px"
-    UlLinks.style.flexDirection = "column"
-    UlLinks.style.zIndex = "5"
-    UlLinks.style.backgroundColor = "white"
-    UlLinks.style.width = "100%"
-    UlLinks.style.transition = "0.4s linear"
+    UlLinks.classList.toggle("active");
+    HeaderIcons.classList.toggle("active")
+})
+
+GoverBox.addEventListener("click" , function () {
+    GoverBox.classList.toggle("active")
 })
