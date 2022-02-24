f0fc67916034235b00dca78a695aa89c145b156e
ahmedwal8d <85419960+ahmedwal8d@users.noreply.github.com>
2022-02-24 18:25:21 +0200

    commit

--- asseds/js/main.js
+++ asseds/js/main.js
@@ -2,18 +2,29 @@ let icon = document.getElementById("icon");
 let HeaderIcon =document.getElementById("header-icon");
 let HeaderIcons = document.getElementById("Header-Icons");
 let UlLinks = document.getElementById("links");
-let UlLink = document.getElementById("link")
-let GoverBox = document.querySelectorAll("#gover-box")
+let ButtonToTop = document.getElementById("to-top");
 
 icon.onclick = function () {
     document.body.classList.toggle("dark-thems")
 }
 
 HeaderIcon.addEventListener("click" , function () {
-    UlLinks.classList.toggle("active");
     HeaderIcons.classList.toggle("active")
+    UlLinks.classList.toggle("active")
 })
 
-GoverBox.addEventListener("click" , function () {
-    GoverBox.classList.toggle("active")
-})
+window.onscroll = function () {
+    if (window.scrollY >= 600) {
+        ButtonToTop.style.opacity = "1"
+    } else {
+        ButtonToTop.style.opacity = "0"
+    }
+}
+
+ButtonToTop.onclick = function () {
+    window.scrollTo({
+        left:0,
+        top:0,
+        behavior: "smooth"
+    })
+}
